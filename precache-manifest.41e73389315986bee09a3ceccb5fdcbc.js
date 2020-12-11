d391fe3cb6c185a10fbe4c76c13c8a66bf0acc5e
Akshay Patil <akshay.patil9111999@gmail.com>
2020-12-11 14:12:02 +0530

    Updates

---
+++ precache-manifest.41e73389315986bee09a3ceccb5fdcbc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce3e47bf2b86cb82f36861a7aaf1a701",
+    "url": "/agro-shop-web/index.html"
+  },
+  {
+    "revision": "96f8148bad0d3d3941d6",
+    "url": "/agro-shop-web/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "71603da20592902941b9",
+    "url": "/agro-shop-web/static/css/main.776ae5d0.chunk.css"
+  },
+  {
+    "revision": "96f8148bad0d3d3941d6",
+    "url": "/agro-shop-web/static/js/2.200a3739.chunk.js"
+  },
+  {
+    "revision": "b84c0c8ded84d686f9a9a77bebeb025c",
+    "url": "/agro-shop-web/static/js/2.200a3739.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71603da20592902941b9",
+    "url": "/agro-shop-web/static/js/main.b48883ee.chunk.js"
+  },
+  {
+    "revision": "7d794c2df32dad82e4cc",
+    "url": "/agro-shop-web/static/js/runtime-main.147cea44.js"
+  },
+  {
+    "revision": "5545ace553c866178260c11be7daf253",
+    "url": "/agro-shop-web/static/media/icon.5545ace5.png"
+  },
+  {
+    "revision": "3a4f6cd4e9d5d4302fb98e536df36af6",
+    "url": "/agro-shop-web/static/media/news.3a4f6cd4.png"
+  }
+]);
